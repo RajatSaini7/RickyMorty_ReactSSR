210b5a1643c9cd20c01a4a8c0f5c8406ddb1f9c7
Rajat Saini <rajsaini2@publicisgroupe.net>
2020-05-18 03:47:34 +0530

    added sidebar filter functionality

--- components/card/index.js
+++ components/card/index.js
@@ -1,62 +1,62 @@
 import React from 'react';
-import Moment from "moment";
+import Moment from 'moment';
 
 const CardComponent = (props) => {
-    const { character } = props;
+  const { character } = props;
 
-    /**
- * @function dateTimeDifference
- *
- * @summary
- * Extracts the difference based on the timestamp provided
- *
- * @param {string} value
- *
- * @returns {string}
- */
-function dateTimeDifference(value) {
-    if (!value && typeof value === "string") {
-      return "";
+  /**
+   * @function dateTimeDifference
+   *
+   * @summary
+   * Extracts the difference based on the timestamp provided
+   *
+   * @param {string} value
+   *
+   * @returns {string}
+   */
+  function dateTimeDifference(value) {
+    if (!value && typeof value === 'string') {
+      return '';
     }
-  
+
     const date = new Date(value);
-  
-    if (date.toString() === "Invalid Date") {
-      return "";
+
+    if (date.toString() === 'Invalid Date') {
+      return '';
     }
-  
+
     //Date conversion for Moment js
     const dateReceived = new Moment(date);
     const currentDate = new Moment(new Date());
-  
+
     if (Moment.duration(currentDate.diff(dateReceived)).asYears() >= 1) {
       return `${parseInt(
         Moment.duration(currentDate.diff(dateReceived)).asYears(),
         10
       )} years ago`;
     }
-  
+
     if (Moment.duration(currentDate.diff(dateReceived)).asMonths() >= 1) {
       return `${parseInt(
         Moment.duration(currentDate.diff(dateReceived)).asMonths(),
         10
       )} months ago`;
     }
-  
+
     if (Moment.duration(currentDate.diff(dateReceived)).asDays() >= 1) {
       return `${parseInt(
         Moment.duration(currentDate.diff(dateReceived)).asDays(),
         10
       )} months ago`;
     }
-  
+
     if (Moment.duration(currentDate.diff(dateReceived)).asHours() >= 1) {
       return `${parseInt(
         Moment.duration(currentDate.diff(dateReceived)).asHours(),
         10
       )} hours ago`;
     }
-  
+
     if (Moment.duration(currentDate.diff(dateReceived)).asMinutes() >= 1) {
       return `${parseInt(
         Moment.duration(currentDate.diff(dateReceived)).asMinutes(),
@@ -64,99 +64,160 @@ function dateTimeDifference(value) {
       )} minutes ago`;
     }
   }
-    return (
-        <>
-            <div className="col-md-3">
-                <div className="card bg-dark text-white mb-4 box-shadow">
-                    <img className="card-img" src={character.image} alt="Card image cap" />
-                    <div className="card-img-overlay">
-                        <h5 className="card-title">{character.name && character.name}</h5>
-                        <span className="card-text">{`id: ${character.id} - created ${dateTimeDifference(character.created)}`}</span>
-                    </div>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <span>STATUS</span>
-                            <span>{character.status && character.status}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span>SPECIES</span>
-                            <span>{character.species && character.species}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span>GENDER</span>
-                            <span>{character.gender && character.gender}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span>ORIGIN</span>
-                            <span>{character.origin && character.origin.name && character.origin.name}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span>LAST LOCATION</span>
-                            <span>{character.location && character.location.name && character.location.name}</span>
-                        </li>
-                    </ul>
-                </div>
+  return (
+    <>
+      <div className="col-6 col-md-3">
+        <div className="card bg-dark text-white mb-4 box-shadow">
+          <div className="img-overlay-container">
+            <img
+              className="card-img"
+              src={character.image}
+              alt="Card image cap"
+            />
+            <div className="card-img-overlay">
+              <h5 className="card-title">{character.name && character.name}</h5>
+              <span className="card-text">{`id: ${
+                character.id
+              } - created ${dateTimeDifference(character.created)}`}</span>
             </div>
-            <style jsx>{`
-             div.card {
-                border-bottom-left-radius: 6px;
-                border-bottom-right-radius: 6px;  
-             }
-             div.card:hover {
-                 cursor: pointer;
-                 border: 2px solid #000000;
-             }
-             .card-img {
-                 border-radius: 0
-             }
-             .card-img-overlay {
-                height: 4em;
-                width: 100%;
-                padding: 7px;
-                background: rgba(0, 0, 0, .6);
-                top: 10.79em;
-             }
-             .card-img-overlay > h5 {
-                margin-bottom: 0;
-                white-space: nowrap;
-                overflow: hidden;
-                text-overflow: ellipsis;
-             }
-             .card-img-overlay > span {
-                font-size: .8em
-             }
-             ul.list-group {
-                background: #333333f2;
-                padding: 14px 14px;
-                border-bottom-left-radius: 6px;
-                border-bottom-right-radius: 6px;
-             }
-             ul.list-group > li {
-                 display: flex;
-                 justify-content: space-between;
-                 background: none;
-                 border-bottom: 1px solid #dacaca24;
-                 border-top: 0;
-                 border-left: 0;
-                 border-right: 0;
-                 padding: .75rem 0;
-             }
-            ul.list-group > li span:first-child {
-                color: #dacacaad;
-                font-size: .7em
-            }
-            ul.list-group > li span:nth-child(2) {
-                color: #b97623eb;
-                font-size: .7em;
-                white-space: pre-wrap;
-                word-break: break-all;
-                padding-left: 6em;
-            }
-       `}
-            </style>
-        </>
-    )
+          </div>
+          <ul className="list-group list-group-flush">
+            <li className="list-group-item">
+              <span>STATUS</span>
+              <span>{character.status && character.status}</span>
+            </li>
+            <li className="list-group-item">
+              <span>SPECIES</span>
+              <span>{character.species && character.species}</span>
+            </li>
+            <li className="list-group-item">
+              <span>GENDER</span>
+              <span>{character.gender && character.gender}</span>
+            </li>
+            <li className="list-group-item">
+              <span>ORIGIN</span>
+              <span>
+                {character.origin &&
+                  character.origin.name &&
+                  character.origin.name}
+              </span>
+            </li>
+            <li className="list-group-item">
+              <span>LAST LOCATION</span>
+              <span>
+                {character.location &&
+                  character.location.name &&
+                  character.location.name}
+              </span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <style jsx>
+        {`
+          div.card {
+            border-bottom-left-radius: 6px;
+            border-bottom-right-radius: 6px;
+          }
+          div.card:hover {
+            cursor: pointer;
+            border: 2px solid #000000;
+          }
+          .card-img {
+            border-radius: 0;
+          }
+          .img-overlay-container {
+            position: relative;
+          }
+          .card-img-overlay {
+            position: absolute;
+            bottom: 0 !important;
+            height: 4em;
+            width: 100%;
+            padding: 7px;
+            background: rgba(0, 0, 0, 0.6);
+            // top: 10.79em;
+            top: unset;
+          }
+          .card-img-overlay > h5 {
+            margin-bottom: 0;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+          }
+          .card-img-overlay > span {
+            font-size: 0.8em;
+          }
+          ul.list-group {
+            background: #333333f2;
+            padding: 14px 14px;
+            border-bottom-left-radius: 6px;
+            border-bottom-right-radius: 6px;
+          }
+          ul.list-group > li {
+            display: flex;
+            justify-content: space-between;
+            background: none;
+            border-bottom: 1px solid #dacaca24;
+            border-top: 0;
+            border-left: 0;
+            border-right: 0;
+            padding: 0.75rem 0;
+          }
+          ul.list-group > li span:first-child {
+            color: #dacacaad;
+            font-size: 0.7em;
+          }
+          ul.list-group > li span:nth-child(2) {
+            color: #b97623eb;
+            font-size: 0.7em;
+            white-space: pre-wrap;
+            word-break: break-all;
+            padding-left: 6em;
+          }
 
+          @media (max-width: 767.98px) {
+            ul.list-group > li span:nth-child(2) {
+              padding-left: 1em;
+            }
+
+            .card-img-overlay {
+              height: 3em;
+              padding: 1px;
+              //   top: 6.79em;
+            }
+
+            .card-img-overlay > h5 {
+              font-size: 0.8em;
+            }
+
+            .card-img-overlay > span.card-text {
+              font-size: 0.5em;
+            }
+          }
+
+          @media (max-width: 991.98px) {
+            ul.list-group > li span:nth-child(2) {
+              padding-left: 1em;
+            }
+
+            .card-img-overlay {
+              height: 3em;
+              padding: 1px;
+            }
+
+            .card-img-overlay > h5 {
+              font-size: 1em;
+            }
+
+            .card-img-overlay > span.card-text {
+              font-size: 0.5em;
+            }
+          }
+        `}
+      </style>
+    </>
+  );
 };
 
 export default CardComponent;
--- components/pagination/index.js
+++ components/pagination/index.js
@@ -1,188 +1,197 @@
 import React, { useState, useEffect } from 'react';
 import _ from 'lodash';
 
-
 const Pagination = (props) => {
-    const { totalRecords = null, pageLimit = 20, pageNeighbours = 0, currPage } = props;
-    const [currentPage, setCurrentPage] = useState(1);
-    const [pages, setPages] = useState([]);
-    const LEFT_PAGE = 'LEFT';
-    const RIGHT_PAGE = 'RIGHT';
+  const {
+    totalRecords = null,
+    pageLimit = 20,
+    pageNeighbours = 0,
+    currPage,
+  } = props;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [pages, setPages] = useState([]);
+  const LEFT_PAGE = 'LEFT';
+  const RIGHT_PAGE = 'RIGHT';
 
-    const totalPages = Math.ceil(totalRecords / pageLimit);
+  const totalPages = Math.ceil(totalRecords / pageLimit);
 
+  // pageLimit = typeof pageLimit === 'number' ? pageLimit : 30;
+  // totalRecords = typeof totalRecords === 'number' ? totalRecords : 0;
 
+  // // pageNeighbours can be: 0, 1 or 2
+  // pageNeighbours = typeof pageNeighbours === 'number'
+  //     ? Math.max(0, Math.min(pageNeighbours, 2))
+  //     : 0;
 
+  useEffect(() => {
+    (async () => {
+      const page = await fetchPageNumbers();
+      // if (page && page.length > 0) {
+      setPages(_.cloneDeep(page));
+      setCurrentPage(currPage);
+    })();
+    // }
+  }, [totalRecords, currentPage]);
 
+  /**
+   * Helper method for creating a range of numbers
+   * range(1, 5) => [1, 2, 3, 4, 5]
+   */
+  const range = (from, to, step = 1) => {
+    let i = from;
+    const range = [];
 
-    // pageLimit = typeof pageLimit === 'number' ? pageLimit : 30;
-    // totalRecords = typeof totalRecords === 'number' ? totalRecords : 0;
+    while (i <= to) {
+      range.push(i);
+      i += step;
+    }
 
-    // // pageNeighbours can be: 0, 1 or 2
-    // pageNeighbours = typeof pageNeighbours === 'number'
-    //     ? Math.max(0, Math.min(pageNeighbours, 2))
-    //     : 0;
+    return range;
+  };
 
-    useEffect(() => {
-        (async () => {
-            const page = await fetchPageNumbers();
-            // if (page && page.length > 0) {
-            setPages(_.cloneDeep(page));
-            setCurrentPage(currPage);
-        })();
-        // }
-    }, [totalRecords, currentPage]);
+  const fetchPageNumbers = () => {
+    const totalPage = totalPages;
+    const currentPages = currentPage;
+    const pageNeighbour = pageNeighbours;
+    // debugger;
 
     /**
-     * Helper method for creating a range of numbers
-     * range(1, 5) => [1, 2, 3, 4, 5]
+     * totalNumbers: the total page numbers to show on the control
+     * totalBlocks: totalNumbers + 2 to cover for the left(<) and right(>) controls
      */
-    const range = (from, to, step = 1) => {
-        let i = from;
-        const range = [];
+    const totalNumbers = pageNeighbour * 2 + 3;
+    const totalBlocks = totalNumbers + 2;
 
-        while (i <= to) {
-            range.push(i);
-            i += step;
+    if (totalPage > totalBlocks) {
+      const startPage = Math.max(2, currentPages - pageNeighbour);
+      const endPage = Math.min(totalPage - 1, currentPages + pageNeighbour);
+
+      let pages = range(startPage, endPage);
+
+      /**
+       * hasLeftSpill: has hidden pages to the left
+       * hasRightSpill: has hidden pages to the right
+       * spillOffset: number of hidden pages either to the left or to the right
+       */
+      const hasLeftSpill = startPage > 2;
+      const hasRightSpill = totalPage - endPage > 1;
+      const spillOffset = totalNumbers - (pages.length + 1);
+
+      switch (true) {
+        // handle: (1) < {5 6} [7] {8 9} (10)
+        case hasLeftSpill && !hasRightSpill: {
+          const extraPages = range(startPage - spillOffset, startPage - 1);
+          pages = [LEFT_PAGE, ...extraPages, ...pages];
+          break;
         }
 
-        return range;
-    }
-
-    const fetchPageNumbers = () => {
-
-        const totalPage = totalPages;
-        const currentPages = currentPage;
-        const pageNeighbour = pageNeighbours;
-        // debugger;
-
-        /**
-         * totalNumbers: the total page numbers to show on the control
-         * totalBlocks: totalNumbers + 2 to cover for the left(<) and right(>) controls
-         */
-        const totalNumbers = (pageNeighbour * 2) + 3;
-        const totalBlocks = totalNumbers + 2;
-
-        if (totalPage > totalBlocks) {
-
-            const startPage = Math.max(2, currentPages - pageNeighbour);
-            const endPage = Math.min(totalPage - 1, currentPages + pageNeighbour);
-
-            let pages = range(startPage, endPage);
-
-            /**
-             * hasLeftSpill: has hidden pages to the left
-             * hasRightSpill: has hidden pages to the right
-             * spillOffset: number of hidden pages either to the left or to the right
-             */
-            const hasLeftSpill = startPage > 2;
-            const hasRightSpill = (totalPage - endPage) > 1;
-            const spillOffset = totalNumbers - (pages.length + 1);
-
-            switch (true) {
-                // handle: (1) < {5 6} [7] {8 9} (10)
-                case (hasLeftSpill && !hasRightSpill): {
-                    const extraPages = range(startPage - spillOffset, startPage - 1);
-                    pages = [LEFT_PAGE, ...extraPages, ...pages];
-                    break;
-                }
-
-                // handle: (1) {2 3} [4] {5 6} > (10)
-                case (!hasLeftSpill && hasRightSpill): {
-                    const extraPages = range(endPage + 1, endPage + spillOffset);
-                    pages = [...pages, ...extraPages, RIGHT_PAGE];
-                    break;
-                }
-
-                // handle: (1) < {4 5} [6] {7 8} > (10)
-                case (hasLeftSpill && hasRightSpill):
-                default: {
-                    pages = [LEFT_PAGE, ...pages, RIGHT_PAGE];
-                    break;
-                }
-            }
-
-            return [1, ...pages, totalPage];
-
+        // handle: (1) {2 3} [4] {5 6} > (10)
+        case !hasLeftSpill && hasRightSpill: {
+          const extraPages = range(endPage + 1, endPage + spillOffset);
+          pages = [...pages, ...extraPages, RIGHT_PAGE];
+          break;
         }
 
-        return range(1, totalPage);
-
-    }
-
-
-    const gotoPage = page => {
-
-        const currentPage = Math.max(0, Math.min(page, totalPages));
-
-        const paginationData = {
-            currentPage,
-            totalPages: totalPages,
-            pageLimit: pageLimit,
-            totalRecords: totalRecords
-        };
-        setCurrentPage(currentPage);
-        props.onPageChanged(paginationData);
-    }
-
-    const handleClick = page => evt => {
-        evt.preventDefault();
-        gotoPage(page);
-    }
-
-    const handleMoveLeft = evt => {
-        evt.preventDefault();
-        gotoPage(currentPage - (pageNeighbours * 2) - 1);
-    }
-
-    const handleMoveRight = evt => {
-        evt.preventDefault();
-        gotoPage(currentPage + (pageNeighbours * 2) + 1);
-    }
-
-
-    return (
-        <>{
-            totalRecords !== 0 && totalPages !== 1 &&
-            <nav aria-label="Characters Pagination">
-                <ul className="pagination">
-                    {pages.map((page, index) => {
-
-                        if (page === LEFT_PAGE) return (
-                            <li key={index} className="page-item">
-                                <a className="page-link" href="#" aria-label="Previous" onClick={handleMoveLeft}>
-                                    <span aria-hidden="true">&laquo;</span>
-                                    <span className="sr-only">Previous</span>
-                                </a>
-                            </li>
-                        );
-
-                        if (page === RIGHT_PAGE) return (
-                            <li key={index} className="page-item">
-                                <a className="page-link" href="#" aria-label="Next" onClick={handleMoveRight}>
-                                    <span aria-hidden="true">&raquo;</span>
-                                    <span className="sr-only">Next</span>
-                                </a>
-                            </li>
-                        );
-
-                        return (
-                            <li key={index} className={`page-item${currentPage === page ? ' active' : ''}`}>
-                                <a className="page-link" href="#" onClick={handleClick(page)}>{page}</a>
-                            </li>
-                        );
-
-                    })}
-
-                </ul>
-            </nav>
+        // handle: (1) < {4 5} [6] {7 8} > (10)
+        case hasLeftSpill && hasRightSpill:
+        default: {
+          pages = [LEFT_PAGE, ...pages, RIGHT_PAGE];
+          break;
         }
-            <style jsx>{`
+      }
+
+      return [1, ...pages, totalPage];
+    }
+
+    return range(1, totalPage);
+  };
+
+  const gotoPage = (page) => {
+    const currentPage = Math.max(0, Math.min(page, totalPages));
+
+    const paginationData = {
+      currentPage,
+      totalPages: totalPages,
+      pageLimit: pageLimit,
+      totalRecords: totalRecords,
+    };
+    setCurrentPage(currentPage);
+    props.onPageChanged(paginationData);
+  };
+
+  const handleClick = (page) => (evt) => {
+    evt.preventDefault();
+    gotoPage(page);
+  };
+
+  const handleMoveLeft = (evt) => {
+    evt.preventDefault();
+    gotoPage(currentPage - pageNeighbours * 2 - 1);
+  };
+
+  const handleMoveRight = (evt) => {
+    evt.preventDefault();
+    gotoPage(currentPage + pageNeighbours * 2 + 1);
+  };
+
+  return (
+    <>
+      {totalRecords !== 0 && totalPages !== 1 && (
+        <nav aria-label="Characters Pagination">
+          <ul className="pagination">
+            {pages.map((page, index) => {
+              if (page === LEFT_PAGE)
+                return (
+                  <li key={index} className="page-item">
+                    <a
+                      className="page-link"
+                      href="#"
+                      aria-label="Previous"
+                      onClick={handleMoveLeft}
+                    >
+                      <span aria-hidden="true">&laquo;</span>
+                      <span className="sr-only">Previous</span>
+                    </a>
+                  </li>
+                );
+
+              if (page === RIGHT_PAGE)
+                return (
+                  <li key={index} className="page-item">
+                    <a
+                      className="page-link"
+                      href="#"
+                      aria-label="Next"
+                      onClick={handleMoveRight}
+                    >
+                      <span aria-hidden="true">&raquo;</span>
+                      <span className="sr-only">Next</span>
+                    </a>
+                  </li>
+                );
+
+              return (
+                <li
+                  key={index}
+                  className={`page-item${
+                    currentPage === page ? ' active' : ''
+                  }`}
+                >
+                  <a className="page-link" href="#" onClick={handleClick(page)}>
+                    {page}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      )}
+      <style jsx>{`
             ul.pagination {
                 margin-top: 0;
                 margin-bottom: 0;
                 box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-                justify-content: flex-end;
+                justify-content: center;
             } 
             ul.pagination > li.page-item.active > a.page-link {
                     color: saturate(darken($base-color, 50%), 5%) !important;
@@ -192,7 +201,7 @@ const Pagination = (props) => {
               
                 ul.pagination > .page-item > a.page-link {
                   padding: 0.75rem 1rem;
-                  min-width: 3.5rem;
+                  min-width: 3rem;
                   text-align: center;
                   box-shadow: none !important;
                   border-color: #ced4da !important;
@@ -204,10 +213,17 @@ const Pagination = (props) => {
                   ul.pagination > .page-item > a.page-link:hover {
                     background-color: lighten(desaturate($base-color, 50%), 12.5%;
                   }
-            `}</style>
-        </>
-    )
-}
 
+                  @media (min-width: 576px) { 
+
+                    ul.pagination {
+                        justify-content: flex-end;
+                    }
+
+                   }
+            `}</style>
+    </>
+  );
+};
 
 export default Pagination;
